40514269b3b615cb669be378cd6916722af383dc
0x01050 <jeraleking@gmail.com>
2020-12-01 20:45:55 +0200

    Remove status info from admins and parameters

--- resources/views/admins.blade.php
+++ resources/views/admins.blade.php
@@ -6,7 +6,6 @@
     <div class="div-root">
         <page-menu v-bind:page="'Administrator'" v-bind:is-admin="'{{ auth()->user()->type == 'administrator' }}'"></page-menu>
         <section>
-            <status-info></status-info>
             <admins></admins>
         </section>
     </div>
--- resources/views/parameters.blade.php
+++ resources/views/parameters.blade.php
@@ -6,7 +6,6 @@
     <div class="div-root">
         <page-menu v-bind:page="'Parameter'" v-bind:is-admin="'{{ auth()->user()->type == 'administrator' }}'"></page-menu>
         <section>
-            <status-info></status-info>
         	<parameters v-bind:is-admin="'{{ auth()->user()->type == 'administrator' }}'"></parameters>
         </section>
     </div>
